5942276e1d50670d6281be24f4db4b1fc06b5d58
sushantnn <sushant.nepal28@gmail.com>
2016-11-30 13:00:55 +0545

    Add files via upload

---
+++ assignments.c
@@ -0,0 +1,26 @@
+/*
+student name:sushant nepal
+subject: programmng fundamental
+labsheet no:08
+program: wap to use assignment operators
+date:
+
+
+*/
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int a,b;
+printf("enter first number");
+scanf("%d",&a);
+printf("enter second numeber");
+scanf("%d",&b);
+a+=b;
+printf("the value is+=%d\n",a);
+getch();
+return(0);
+}
