5cf71148ac78cfbb1f145658b1c022551ee55543
sushantnn <sushant.nepal28@gmail.com>
2016-12-14 10:48:44 +0545

    Add files via upload

---
+++ aaaaa.c
@@ -0,0 +1,29 @@
+/*
+name=sushant nepal
+roll=42
+subject=programming fundamental
+program=write c c program to check whether the number is divisible by 5&11 or not
+*/
+
+#include<stdio.h>;
+#include<conio.h>;
+int main(){
+int a;
+printf("Enter the number");
+scanf("%d",&a);
+
+if (a%5==0 && as%11==0)
+{
+printf("the number is divisible by 5&11");
+}
+else
+    {
+    printf("the number is not divisible by 5&11");
+    }
+getch();
+return(0);
+
+
+
+
+}
